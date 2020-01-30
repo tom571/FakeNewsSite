9369367476f9ad30399a61483e30d823b842f0a3
Tom <tafolder@gmail.com>
2020-01-30 20:45:19 +0000

    FakeNewsSite Initial Commit

---
+++ scripts.js
@@ -0,0 +1,21 @@
+/*var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("nav-container").style.top = "0";
+  } else {
+    document.getElementById("nav-container").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+} */
+
+
+//const burgerClick = (x) => (x.classlist.toggle('change');
+
+function burgerAnimate(x) {
+  
+  x.classList.toggle("change");
+
+  //const navBox = document.getElementById('nav-toggle');
+  //let navChecked = navBox.checked ? navBox.checked = false : navBox.checked = true;
+}

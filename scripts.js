c3fff49e495ea8f60b80320771fa60cc34c33b33
Tom <tafolder@gmail.com>
2020-02-01 22:14:51 +0000

    added transition on nav bar

--- scripts.js
+++ scripts.js
@@ -1,9 +1,10 @@
 
 const elNavContainer = document.getElementById("nav-container");
+const elLeftMenu = document.getElementById("left-menu");
 let prevScrollPos = window.pageYOffset;
 
 window.addEventListener("scroll", () => { //using event listener instead of .onscroll as if use .onscroll again will override it
-  elNavContainer.style.display = prevScrollPos > window.pageYOffset ? "block" : "none";
+  elNavContainer.style.top = prevScrollPos > window.pageYOffset ? "0" : "-70px";
   prevScrollPos = window.pageYOffset;
 }); 
 
